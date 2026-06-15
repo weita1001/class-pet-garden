d4a51dbbd7a4349bbfcfeffce4a1e046d0fe4c50
Teacher <teacher@classpet.local>
2026-06-15 22:15:00 +0800

    宠物园静态部署文件

---
+++ railway-deploy/sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', () => { self.skipWaiting() })
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys()
+    await Promise.all(keys.map(k => caches.delete(k)))
+    await self.registration.unregister()
+  })())
+})
